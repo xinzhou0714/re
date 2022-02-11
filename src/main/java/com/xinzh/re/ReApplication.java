1f742206e7fc4e536427366c7bbe4be2684de87a
xinzhou0714 <zhouxin0714@gmail.com>
2022-02-11 23:19:02 +0100

    chore(integrate Logger und configurate server port):

--- src/main/java/com/xinzh/re/ReApplication.java
+++ src/main/java/com/xinzh/re/ReApplication.java
@@ -1,13 +1,21 @@
 package com.xinzh.re;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ReApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(ReApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(ReApplication.class, args);
+        SpringApplication app = new SpringApplication(ReApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Spring Boot Sever started");
+        LOG.info("Address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
     }
 
 }
